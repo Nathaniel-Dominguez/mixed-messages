606c0845b7a261a19f83fd0dfbcc463f6e3a4d3b
keppy <keppy.rb@gmail.com>
2022-02-22 14:53:35 -1000

    this code has an obvious problem, can you see it?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const messages = [
-	'Hello There', 
-	'You Underestimate My Power', 
+	'Hello There',
+	'You Underestimate My Power',
 	'Don\'t Try It',
 	'This is the Way',
 	'May the Force be with You',
@@ -15,3 +15,21 @@ const randomMessage = () => {
 	let random = messages[Math.floor(Math.random(messages) * messages.length)];
 	document.getElementById('text').innerHTML = random;
 };
+
+// Make a news feed using FIFO(first in first out) queue so that items are processed
+// and then removed from the queue in order. https://stackoverflow.com/a/35031174 <- FIFO vs LIFO
+let newsQueue = [];
+// The default RSS page for reddit is 25 items
+const queueLength = 25;
+const offbeatURI = 'https://www.reddit.com/r/offbeat/new/.rss?sort=new';
+
+const fetchNews = () => {
+    fetch(offbeatURI)
+        .then(res => res.text())
+        .then(txt => new window.DOMParser().parseFromString(txt, 'text/html'))
+        .then(data => {
+            console.log(data)
+            const items = data.querySelectorAll("item")
+            console.log(items)
+        })
+}
